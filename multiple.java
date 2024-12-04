7b0c21ba2fa0128d7160e6bf2737a9180b4bd3ec
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-04 13:24:55 +0530

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class multiple
+{
+    public static void main(String args[])
+{
+    int n=25;
+    if((n&3)==0)
+    {
+        System.out.println("yes");
+    }
+    else
+    {
+        System.out.println("no");
+    }
+}
+}
