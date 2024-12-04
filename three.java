f447be91a5d6e70e0c9b8d74dd408f487542a015
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-04 15:17:57 +0530

    Add files via upload

---
+++ three.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class three
+{
+    public static void main(String args[])
+    {
+        int a=20;
+        int b=98;
+        int c=9;
+      if((a>b)&&(a>c))
+      System.out.println("a is bigger");
+      else if((b>a)&&(b>c))
+      System.out.println("b is bigger");
+      else
+      System.out.println("c is bigger"); 
+    }
+}
