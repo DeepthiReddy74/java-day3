0734b738b3766ed47863189d4573d495766c4f00
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-04 13:25:46 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Power{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+int n=Sc.nextInt();
+if((n&1)==0)
+{
+    System.out.println("even");
+}
+else
+{
+    System.out.println("odd");
+}
+}
+}
