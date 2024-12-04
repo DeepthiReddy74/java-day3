91881fb16846aed1fe5524d43ad96d8c66575adf
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-04 13:23:09 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class A{
+    public static void main (String[]args){
+        int a=10;
+        int pos=1;
+        if((a&(1<<pos))!=0){
+            System.out.println("set bit");
+        }else{
+            System.out.println("not a set bit");
+        
+        }
+    }
+
+}
