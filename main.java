6b528f67f92c7d258f2cee5e8102a753e7696885
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-04 13:24:02 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        int a=64;
+        if((a&(a-1))==0)
+        {
+            System.out.println("power of 2");
+        }
+        else
+        {
+            System.out.println("not power of 2");
+        }
+        }
+    }
